97ea902f42a6a17e00c732ba779e1daa69c68e9e
Илья <ilacerepanov078@gmail.com>
2025-10-07 12:02:45 +0300

    Add files via upload

---
+++ DKR_1_1.py
@@ -0,0 +1,11 @@
+import math
+x = float(input('Введите значение x: '))
+if x < -6:
+    y = math.sin(x) * x**2 - x**2
+elif -6 <= x < 1:
+    y = 32 / (x**(1/3)) - math.sin(x) / 20
+    if -6 <= x < 0:
+        y = "не определено"
+else:
+    y = -x * (math.sin(x) / math.cos(x))
+print('Значение функции = ', y)
