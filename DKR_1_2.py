3607a6342c13406b4befa1baba47e83e6ddfb444
Илья <ilacerepanov078@gmail.com>
2025-10-16 15:03:50 +0300

    Update DKR_1_2.py

--- DKR_1_2.py
+++ DKR_1_2.py
@@ -4,11 +4,12 @@ x = -8.0
 while x <= 3.2:
     if x < -6:
         y = math.sin(x) * x ** 2 - x ** 2
-    elif -6 <= x < 1:
+    elif x < 1:
         y = 32 / (x ** (1 / 3)) - math.sin(x) / 20
         if -6 <= x < 0:
             y = 'не определено'
     else:
         y = -x * (math.sin(x) / math.cos(x))
     print('x = ', round(x,2), 'y = ', y)
+
     x += 0.2
