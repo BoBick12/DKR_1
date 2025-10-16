a0b3961b80e331411ca9a16ec327e3d560be1d07
Илья <ilacerepanov078@gmail.com>
2025-10-16 15:02:21 +0300

    Add files via upload

---
+++ ДКР_1(1).pas
@@ -0,0 +1,12 @@
+﻿var x, y:real;
+begin
+  writeln('Введите значение x');
+  readln(x);
+  if x<-6 then
+    y:= sin(x) * x**2 - x**2
+  else if x < 1 then 
+    y:= 32 / x**(1/3) - sin(x) / 20
+  else 
+    y:= -x*(sin(x)/cos(x));
+  writeln('Значение функции = ',y:0:2)
+end.
